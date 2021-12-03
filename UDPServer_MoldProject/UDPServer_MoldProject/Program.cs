d8c5f990cd5dad26bc874305d57493cc73f2db87
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-12-03 13:04:38 +0100

    UDPConsolApp

---
+++ UDPServer_MoldProject/UDPServer_MoldProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDPServer_MoldProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
