609735509536990c87bee7c1728d646dabd68377
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-12-03 13:55:00 +0100

    Server

---
+++ UDPServer_MoldProject/UDPServer_MoldProject/DeviceHumidityTemperature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPServer_MoldProject
+{
+    public class DeviceHumidityTemperature
+    {
+        public DeviceHumidityTemperature()
+        {
+        }
+
+        public int DeviceNo { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+
+        public DeviceHumidityTemperature(int deviceNo, double temperature, double humidity)
+        {
+            DeviceNo = deviceNo;
+            Temperature = temperature;
+            Humidity = humidity;
+        }
+
+
+    }
+}
