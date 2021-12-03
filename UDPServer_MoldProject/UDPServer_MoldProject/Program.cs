b9b856990eb5c8e5bb51dbd7e52e76d8e30a01d3
Vladimir Sumilin <vladimirshumilin42@gmail.com>
2021-12-03 13:16:34 +0100

    Console app

--- UDPServer_MoldProject/UDPServer_MoldProject/Program.cs
+++ UDPServer_MoldProject/UDPServer_MoldProject/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 namespace UDPServer_MoldProject
 {
@@ -6,7 +9,27 @@ namespace UDPServer_MoldProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("UDP Server");
+            StartListener();
+        }
+
+        public static void StartListener()
+        {
+            using (UdpClient socket = new UdpClient())
+            {
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 9999));
+
+                while (true)
+                {
+                    IPEndPoint from = null;
+                    byte[] data = socket.Receive(ref from);
+                    string recieved = Encoding.UTF8.GetString(data);
+                    Console.WriteLine("Server receieved: " + recieved);
+                    Console.WriteLine("Client address: " + from.Address);
+
+                    socket.Send(data, data.Length, from);
+                }
+            }
         }
     }
 }
