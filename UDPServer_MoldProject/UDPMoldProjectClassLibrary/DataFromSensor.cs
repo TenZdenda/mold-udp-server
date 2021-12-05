b90db06ed3959ae15d4dd789dea9ed6898bc97d7
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-12-05 22:22:51 +0100

    UDPServer

---
+++ UDPServer_MoldProject/UDPMoldProjectClassLibrary/DataFromSensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPMoldProjectClassLibrary
+{
+    class DataFromSensor
+    {
+        public int DeviceNo { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
--- UDPServer_MoldProject/UDPServer_MoldProject/Program.cs
+++ UDPServer_MoldProject/UDPServer_MoldProject/Program.cs
@@ -2,34 +2,42 @@
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Net.Http;
 
-namespace UDPServer_MoldProject
+namespace UDPMoldProjectListener
 {
     class Program
     {
+        private static string APIURL = "http://localhost:23864/api/MoldProjectAPI";
         static void Main(string[] args)
         {
-            Console.WriteLine("UDP Server");
-            StartListener();
-        }
+            Console.WriteLine("UDP Listener");
 
-        public static void StartListener()
-        {
+            
             using (UdpClient socket = new UdpClient())
             {
-                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 9999));
-
-                while (true)
-                {
-                    IPEndPoint from = null;
-                    byte[] data = socket.Receive(ref from);
-                    string recieved = Encoding.UTF8.GetString(data);
-                    Console.WriteLine("Server receieved: " + recieved);
-                    Console.WriteLine("Client address: " + from.Address);
-
-                    socket.Send(data, data.Length, from);
+                
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 10100));
+                                
+                using (HttpClient client = new HttpClient())
+                {                   
+                    while (true)
+                    {                        
+                        IPEndPoint from = null;
+                       
+                        byte[] data = socket.Receive(ref from);
+                        
+                        string recieved = Encoding.UTF8.GetString(data);
+                        Console.WriteLine("Server receieved: " + recieved + " From " + from.Address);
+                                                
+                        HttpContent content = new StringContent(recieved, Encoding.UTF8, "application/json");
+                        
+                        client.PostAsync(APIURL, content);
+                    }
                 }
             }
         }
+
+        
     }
 }
